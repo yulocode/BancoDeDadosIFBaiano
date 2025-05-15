e5bada10532947b49aeece2ac2c93a4d3a128343
$yulo <101001858+yulocode@users.noreply.github.com>
2025-05-15 11:31:11 -0300

    Create atividade.sql

---
+++ atividade.sql
@@ -0,0 +1,44 @@
+CREATE TABLE Funcionário 
+( 
+ Nome INT,  
+ CPF INT PRIMARY KEY,  
+ Cargo INT,  
+ idEmpresa INT,  
+ idDepartamento INT,  
+); 
+
+CREATE TABLE Departamento 
+( 
+ Prédio INT,  
+ Nome INT,  
+); 
+
+CREATE TABLE Projeto 
+( 
+ idCliente INT,  
+ idEmpresa INT,  
+); 
+
+CREATE TABLE Cliente 
+( 
+ CNPJ INT PRIMARY KEY,  
+ Nome INT,  
+ Telefone INT,  
+); 
+
+CREATE TABLE Empresa 
+( 
+); 
+
+CREATE TABLE Alocação 
+( 
+ idEmpresa INT PRIMARY KEY,  
+ idDepartamento INT,  
+); 
+
+ALTER TABLE Funcionário ADD FOREIGN KEY(idEmpresa) REFERENCES Empresa (idEmpresa)
+ALTER TABLE Funcionário ADD FOREIGN KEY(idDepartamento) REFERENCES Departamento (idDepartamento)
+ALTER TABLE Projeto ADD FOREIGN KEY(idCliente) REFERENCES Cliente (idCliente)
+ALTER TABLE Projeto ADD FOREIGN KEY(idEmpresa) REFERENCES Empresa (idEmpresa)
+ALTER TABLE Alocação ADD FOREIGN KEY(idEmpresa) REFERENCES Empresa (idEmpresa)
+ALTER TABLE Alocação ADD FOREIGN KEY(idDepartamento) REFERENCES Departamento (idDepartamento)
